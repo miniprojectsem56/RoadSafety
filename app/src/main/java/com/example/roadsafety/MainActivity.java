9fb434ec41d35d951b64f22b1047f6bb4ae4c73d
Bhargav760 <72614126+Bhargav760@users.noreply.github.com>
2021-10-18 10:29:09 +0530

    TabbedActivityCreated

--- app/src/main/java/com/example/roadsafety/MainActivity.java
+++ app/src/main/java/com/example/roadsafety/MainActivity.java
@@ -1,31 +1,64 @@
 package com.example.roadsafety;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
-    private Button registration,roadStatus;
+
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter adapter;
+    Button button;
+
+    int cnt=0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        registration=findViewById(R.id.registration);
-        roadStatus=findViewById(R.id.roadStatus);
 
-        registration.setOnClickListener(new View.OnClickListener() {
+        tabLayout= findViewById(R.id.Tablayout);
+        viewPager2= findViewById(R.id.Viewpager2);
+
+        FragmentManager fm=getSupportFragmentManager();
+        adapter=new FragmentAdapter(fm,getLifecycle());
+        viewPager2.setAdapter(adapter);
+
+        Log.d("Item","Creayed");
+
+        tabLayout.addTab(tabLayout.newTab().setText("Map"));
+        tabLayout.addTab(tabLayout.newTab().setText("Explore"));
+        tabLayout.addTab(tabLayout.newTab().setText("Safest roads"));
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
-            public void onClick(View v) {
-                
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager2.setCurrentItem(tab.getPosition());
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
             }
         });
 
-        roadStatus.setOnClickListener(new View.OnClickListener() {
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
             @Override
-            public void onClick(View v) {
-
+            public void onPageSelected(int position) {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
             }
         });
     }
