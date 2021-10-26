ed847ca9644737200e900ed88749030be2568780
Bhargav760 <72614126+Bhargav760@users.noreply.github.com>
2021-10-26 13:09:32 +0530

    Firebase CONNECTED

--- app/src/main/java/com/example/roadsafety/CrearteAccountActivity.java
+++ app/src/main/java/com/example/roadsafety/CrearteAccountActivity.java
@@ -1,20 +1,30 @@
 package com.example.roadsafety;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CrearteAccountActivity extends AppCompatActivity {
 
     EditText name_of_person,email_of_person,created_password,confirmed_password;
     Button create_account;
     Intent toMainActivity;
-    @Override
+    FirebaseAuth auth;
+    ProgressDialog progressDialog;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crearte_account);
@@ -24,19 +34,65 @@ public class CrearteAccountActivity extends AppCompatActivity {
         created_password = findViewById(R.id.create_password);
         confirmed_password = findViewById(R.id.confirm_password);
         create_account = findViewById(R.id.create_button);
-        toMainActivity = new Intent(CrearteAccountActivity.this,MainActivity.class);
+        toMainActivity = new Intent(CrearteAccountActivity.this,LoginActivity.class);
 
+        progressDialog = new ProgressDialog(CrearteAccountActivity.this);
+        progressDialog.setTitle("new Account");
+        progressDialog.setMessage("Creating new Account");
+
+        auth = FirebaseAuth.getInstance();
         create_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(created_password.toString()==""||created_password.toString()!=confirmed_password.toString()||name_of_person.toString()==""||email_of_person.toString()=="")
+                progressDialog.show();
+                progressDialog.setCancelable(false);
+                progressDialog.setCanceledOnTouchOutside(false);
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                if(created_password.toString()==""||name_of_person.toString()==""||email_of_person.toString()=="")
                 {
                     Toast.makeText(CrearteAccountActivity.this,"Enter valid details",Toast.LENGTH_SHORT).show();
+                    progressDialog.hide();
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
                 }
                 else
                 {
-                    Toast.makeText(CrearteAccountActivity.this,"Account created",Toast.LENGTH_SHORT).show();
-                    startActivity(toMainActivity);
+                    auth.createUserWithEmailAndPassword(email_of_person.getText().toString(),created_password.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()) {
+                                String uid = task.getResult().getUser().getUid();
+                                progressDialog.setMessage("Account Created Successfully");
+
+                                auth.getCurrentUser().sendEmailVerification().
+                                        addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if(task.isSuccessful()) {
+                                                    progressDialog.hide();
+                                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                                                    startActivity(toMainActivity);
+                                                }
+                                                else {
+                                                    Toast.makeText(CrearteAccountActivity.this, task.getException().getMessage()
+                                                            , Toast.LENGTH_SHORT).show();
+                                                    progressDialog.hide();
+                                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                                                }
+                                            }
+
+                                        });
+                            }
+                            else
+                            {
+                                Toast.makeText(CrearteAccountActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                progressDialog.hide();
+                                getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                            }
+                        }
+                    });
+
                 }
             }
         });
--- app/src/main/java/com/example/roadsafety/LoginActivity.java
+++ app/src/main/java/com/example/roadsafety/LoginActivity.java
@@ -1,28 +1,42 @@
 package com.example.roadsafety;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
     EditText name;
     EditText password;
     Button loginButton,signIn;
-
-    String nameAtFirebase = "Bhargav";
-    String passwordAtFirebase = "Bhargav@123";
-
+    ProgressDialog progressDialog;
+    FirebaseAuth auth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null) {
+            startActivity((new Intent(LoginActivity.this, MainActivity.class)));
+            finish();
+        }
 
+        progressDialog = new ProgressDialog(LoginActivity.this);
+        progressDialog.setTitle("LogIn");
+        progressDialog.setMessage("Signing in please wait");
         name = findViewById(R.id.username_et);
         password = findViewById(R.id.password_et);
         loginButton = findViewById(R.id.login_button);
@@ -41,24 +55,41 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                progressDialog.show();
+                progressDialog.setCancelable(false);
+                progressDialog.setCanceledOnTouchOutside(false);
+                getWindow().setFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE,
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
                 String inputted_name = name.getText().toString();
                 String inputted_password = password.getText().toString();
                 if(inputted_name.length()==0||inputted_password.length()==0)
                 {
                     Toast.makeText(LoginActivity.this,"Fill everything correctly",Toast.LENGTH_LONG).show();
+                    progressDialog.hide();
+                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
                 }
-                else if(!inputted_password.equals(passwordAtFirebase))
-                {
-                    Toast.makeText(LoginActivity.this,"Fill password correctly",Toast.LENGTH_LONG).show();
+                else{
+                    auth.signInWithEmailAndPassword(inputted_name, inputted_password).
+                            addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                if (auth.getCurrentUser().isEmailVerified()) {
+                                    progressDialog.hide();
+                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                                    startActivity(LoginValidated);
+                                }
+                                else {
+                                    progressDialog.hide();
+                                    getWindow().clearFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
+                                    Toast.makeText(LoginActivity.this, "Incorrect email or password",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
                 }
-                else
-                {
-                    startActivity(LoginValidated);
-                }
-
             }
-
         });
     }
 }
